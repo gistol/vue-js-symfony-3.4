c9dc89fa9f8e384e152d2f7ca6197fb505c5c1b7
florenta <florent.alemany@gmail.com>
2018-05-05 01:23:47 +0200

    Updated app.js file [put store in another folder]

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,6 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
-import Router from './router'
-
-Vue.use(Vuex);
-
-const Store = new Vuex.Store({
-    state:{
-        articles: []
-    },
-    mutations: {
-
-    },
-    actions: {
-        postData(context, data) {
-
-            const req = window.XMLHttpRequest?
-                new XMLHttpRequest() :
-                new ActiveXObject("Microsoft.XMLHTTP");
-
-            req.open("POST", data.url, true);
-            req.addEventListener("load", () => {
-                if (req.status >= 200 && req.status < 400) {
-                    alert(req.responseText)
-                }
-            });
-
-            if (data.contentType) {
-                req.setRequestHeader("Content-Type", data.contentType);
-            }
-
-            req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-            req.send(data.value);
-        }
-    }
-});
+import Router from './router';
+import Store from './store';
 
 new Vue({
     el: "#app",
