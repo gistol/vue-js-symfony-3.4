6bd849b8347a96bdd90de6bb5154528b39742a0b
florenta <florent.alemany@gmail.com>
2018-05-05 23:18:33 +0200

    Added an hydrateObject method and added csrf_token check

--- src/AppBundle/Service/Hydrator.php
+++ src/AppBundle/Service/Hydrator.php
@@ -2,14 +2,35 @@
 
 namespace AppBundle\Service;
 
-use Symfony\Component\BrowserKit\Response;
+use AppBundle\Entity\Article;
+use AppBundle\Entity\Image;
 
 class Hydrator extends MetaService
 {
+    /**
+     * @var \stdClass
+     */
+    private $class;
+
+    /**
+     * Function to check whether the form is conform or not
+     * Added field in comparison to the specified entity will result in
+     * returning false as well as a wrong csrf_token
+     *
+     * @param $class
+     * @return bool
+     */
     public function isFormValid($class)
     {
-        foreach ($this->request->request as $field => $value) {
-            if ($field !== "csrf_token") {
+        $this->class = $class;
+
+        foreach ($this->getRequest()->request as $field => $value) {
+
+            if ($field === 'csrf_token') {
+                if (!($value === $this->getSession()->get('csrf_token'))) {
+                    return false;
+                }
+            } else {
                 if (!method_exists(new $class, ($method = 'get' . ucfirst($field)))) {
                     return false;
                 }
@@ -17,5 +38,42 @@ class Hydrator extends MetaService
         }
 
         return true;
+    }
+
+    /**
+     * Function to hydrate the specified object
+     * Not need to validate fields as already done
+     *
+     * @param $class
+     * @return object
+     */
+    public function hydrateObject()
+    {
+        $object = new $this->class;
+
+        foreach ($this->getRequest()->request as $field => $value) {
+            if ($field !== "csrf_token") {
+                $method = 'set' . ucfirst($field);
+                $object->$method(htmlspecialchars($value));
+            }
+        }
+
+        if ($object instanceof Article) {
+            $article = &$object;
+            $article->setDate(new \DateTime());
+
+            if (!is_null($file = $this->request->files->get('image'))) {
+                $filename = uniqid() . '.' . $file->guessExtension();
+
+                $image = new Image();
+                $image->setSrc($filename);
+                $image->setTitle($article->getTitle());
+                $image->setArticle($article);
+                $article->addImage($image);
+                $file->move($this->container->getParameter('images_directory'), $filename);
+            }
+        }
+
+        return $object;
     }
 }
