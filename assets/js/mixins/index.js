dce62bdee83aaa5ae58fed37b3693f8ca56346fd
florenta <florent.alemany@gmail.com>
2018-05-13 10:26:15 +0200

    Moved method into mixins

--- assets/js/mixins/index.js
+++ assets/js/mixins/index.js
@@ -1,6 +1,17 @@
 const Mixins = {
     methods: {
-
+        getArticles() {
+            fetch('/vue-js-symfony-3.4/web/app_dev.php/articles')
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.length > 0) {
+                        this.articles = data;
+                    }
+                })
+                .catch((err) => {
+                    console.log('Erreur : ' + err)
+                })
+        }
     }
 };
 
