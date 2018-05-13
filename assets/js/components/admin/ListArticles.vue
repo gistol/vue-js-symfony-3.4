0d1310ab6d9c0634dd0f9cde1181ce0516e0b97a
florenta <florent.alemany@gmail.com>
2018-05-13 19:13:59 +0200

    Added delete article method in ListArticles component

--- assets/js/components/admin/ListArticles.vue
+++ assets/js/components/admin/ListArticles.vue
@@ -5,6 +5,19 @@
                 <tr>
                     <td> {{ article.title }} </td>
                 </tr>
+                <tr>
+                    <td>
+                        <img :src="'./images/' + article.images[0].src"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button v-on:click="deleteArticle" :data-id="article.id">Supprimer</button>
+                        <router-link  v-bind:to="{name: 'editArticle', params: {slug: article.slug}}">
+                            <button>Editer</button>
+                        </router-link>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -22,6 +35,22 @@
         },
 
         mixins: [Mixin],
+
+        methods: {
+            deleteArticle(e) {
+
+                this.$store.dispatch('postData', {
+                   url: '/vue-js-symfony-3.4/web/app_dev.php/admin/articles/delete/' + e.target.getAttribute('data-id')
+                })
+                .then((data) => {
+                    console.log(data);
+                    e.target.parentNode.parentNode.parentNode.parentNode.remove();
+                })
+                .catch((err) => {
+                    console.log("Erreur : " + err);
+                });
+            }
+        },
 
         beforeMount() {
             this.getArticles();
