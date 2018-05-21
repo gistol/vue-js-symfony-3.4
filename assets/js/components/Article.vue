abaf4b203192f1918df8e9a3f0b225fe6cb2d72d
florenta <florent.alemany@gmail.com>
2018-05-21 11:22:35 +0200

    Mise en forme

--- assets/js/components/Article.vue
+++ assets/js/components/Article.vue
@@ -1,33 +1,40 @@
 <template>
-    <div class='container'>
-        <figure v-for="image in article.images">
-            <img :src="'./images/' + image.src" :alt='image.title' />
-            <figcaption>{{ image.content }}</figcaption>
-        </figure>
+    <div>
+        <div class='container'>
+            <figure v-for="image in article.images">
+                <img :src="'./images/' + image.src" :alt='image.title' />
+                <figcaption>{{ image.content }}</figcaption>
+            </figure>
+        </div>
+        <div id="comment_modal" v-show="show">
+            <div v-bind:style="width">
+                <button @click="showForm" class="button-close">Fermer</button>
+                <form v-on:submit.prevent="handleComment" enctype="multipart/form-data">
 
-        <form v-show="show" v-on:submit.prevent="handleComment" enctype="multipart/form-data">
+                    <div>
+                        <label for="username">Nom</label>
+                        <input type="text" id="username" name="username" v-model="username"/>
+                    </div>
 
-            <div>
-                <label for="username">Nom</label>
-                <input type="text" id="username" name="username" v-model="username"/>
+                    <div>
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" v-model="email"/>
+                    </div>
+
+                    <div>
+                        <label for="comment">Commentaire</label>
+                        <textarea id="comment" name="comment" v-model="comment"></textarea>
+                    </div>
+
+                    <input type="hidden" name="csrf_token" v-bind:value='csrf_token'/>
+
+                    <input type="submit" class="button-submit"/>
+                </form>
             </div>
-
-            <div>
-                <label for="email">Email</label>
-                <input type="text" id="email" name="email" v-model="email"/>
-            </div>
-
-            <div>
-                <label for="comment">Commentaire</label>
-                <input type="text" id="comment" name="comment" v-model="comment"/>
-            </div>
-
-            <input type="hidden" name="csrf_token" v-bind:value='csrf_token'/>
-
-            <input type="submit"/>
-
-        </form>
-        <button @click="showForm">Commenter</button>
+        </div>
+        <div class='container'>
+            <button ref='modal_opener' @click="showForm" class="button-link">Commenter</button>
+        </div>
     </div>
 </template>
 
@@ -45,14 +52,17 @@
                 email: undefined,
                 comment: undefined,
                 article: undefined,
-                show: false
+                show: false,
+                width: {
+                    width: '40%'
+                }
             }
         },
 
         computed: {
             csrf_token() {
                 return this.$store.state.csrf_token;
-            }
+            },
         },
 
         mixins: [Mixin],
@@ -76,19 +86,64 @@
 
             showForm(e) {
                 this.show = !this.show;
-                e.target.innerText = this.show ? "Masquer" : "Commenter";
+                this.$refs.modal_opener = this.show ? "Masquer" : "Commenter";
             }
         }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+    @import "../../../assets/css/variables";
+
     .container {
         width: 50%;
+
+        img {
+            @extend %max-width;
+        }
     }
 
-    img{
-        width: 100%;
+    #comment_modal {
+        position: fixed;
+        background: rgba(0,0,0,.8);
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        @extend %flex;
+        @extend %align-center;
+        @extend %justify-center;
+
+        button {
+            position: absolute;
+            top: 5px;
+            right: 5px;
+        }
+
+        form {
+
+            padding: 10px;
+
+            background: $light-grey;
+
+            /* This rule overwrites the one defined in variables.scss only for width */
+            @include loopItem("input[type=text]", 'width', 90%);
+
+            textarea {
+                height: 200px;
+                width: 90%;
+                resize: none;
+                padding: 5px;
+            }
+
+            input[type='submit'] {
+                margin: 10px auto;
+            }
+        }
     }
 
+    button {
+        margin: 10px auto;
+    }
 </style>
--- assets/js/components/User.vue
+++ assets/js/components/User.vue
@@ -1,17 +1,17 @@
 <template>
-    <div id="app">
+    <div>
         <nav>
             <ul>
                 <router-link v-bind:to="{name: 'home_user'}" tag="li">Accueil</router-link>
             </ul>
         </nav>
-        <h1 v-if="this.$route.name === 'home_user'">Accueil</h1>
+        <h1 v-if="this.$route.name === 'home_user'">Côté Desserts</h1>
         <router-view></router-view>
         <footer>
             <form v-on:submit.prevent="addToNewsletter" :enctype="enctype">
                 <label for="email"></label>
                 <input type="email" v-model="newsletter" name="email" ref="newsletter" id="email" placeholder="Votre email"/>
-                <input type="submit" value="Envoyer"/>
+                <input type="submit" value="Je m'abonne" class="button-submit"/>
             </form>
         </footer>
     </div>
@@ -23,6 +23,5 @@
     export default {
         name: 'User',
         mixins: [Mixin]
-
     }
 </script>
