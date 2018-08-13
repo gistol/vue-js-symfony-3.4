28e238528c976aa6673702c230d8b31fb4f83a25
florenta <florent.alemany@gmail.com>
2018-08-13 19:50:10 +0200

    Page not found - added smiley

--- assets/js/components/PageNotFound.vue
+++ assets/js/components/PageNotFound.vue
@@ -1,13 +1,27 @@
 <template>
-    <div class="container">
+    <div class="container w40 w95sm text-centered">
         <p>Oops, page non trouvée ! </p>
+        <font-awesome-icon v-bind:icon="frownIcon" size="6x"/>
     </div>
 </template>
 
 <script>
 
+    import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+    import faFrown from '@fortawesome/fontawesome-free-solid/faFrown';
+
     export default {
         name: 'PageNotFound',
+
+        data() {
+            return {
+                frownIcon: faFrown
+            }
+        },
+
+        components: {
+            FontAwesomeIcon
+        },
 
         mounted() {
 
