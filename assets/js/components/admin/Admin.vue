2c8b482f19f83bb59a87506b6062de94fc441ad1
florenta <florent.alemany@gmail.com>
2018-08-13 19:57:11 +0200

    Admin - menu display for small devices

--- assets/js/components/admin/Admin.vue
+++ assets/js/components/admin/Admin.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <nav id="admin_navbar">
-            <img v-if='smallDevice' v-on:click='showMenu' id="burger" src="images/threelines.png" />
-            <ul id="menu">
+            <div id="burger" v-if='smallDevice' v-on:click="showMenu">
+                <font-awesome-icon style="color: #fff;" v-bind:icon="barsIcon" size="lg"/>
+            </div>
+            <ul id="menu" v-on:click="resetMenu()"> <!-- menu not used in css but in javascript -->
                 <router-link v-bind:to="{name: 'createArticle'}" tag="li">Créer un article</router-link>
                 <router-link v-bind:to="{name: 'homepageAdmin'}" tag="li">Liste des articles</router-link>
                 <router-link v-bind:to="{name: 'comments'}" tag="li">Commentaires</router-link>
@@ -23,14 +25,26 @@
     import Mixin from '../../mixins/index';
     import MenuMixin from '../../mixins/menuMixin';
     import { mapState } from 'vuex';
+    import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+    import faBars from '@fortawesome/fontawesome-free-solid/faBars';
 
     export default {
 
         name: 'Admin',
 
+        data() {
+            return {
+                barsIcon: faBars,
+            }
+        },
+
         mixins: [Mixin, MenuMixin],
 
         computed: mapState(['displayMessage', 'message']),
+
+        components: {
+            FontAwesomeIcon
+        },
 
         beforeRouteUpdate(to, from, next) {
             if (to.name === 'logout') {
